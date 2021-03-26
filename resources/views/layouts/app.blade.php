5803349566b76bf0736b5cb342233121a12f6559
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-03-26 20:30:01 +0300

    Layout yapısı düzenlendi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,15 +28,21 @@
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            {{ $header }}   
+                        </h2>
                     </div>
                 </header>
             @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <div class="py-6">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        {{ $slot }}
+                    </div>
+                </div>
+            
+                
+            
         </div>
 
         @stack('modals')
