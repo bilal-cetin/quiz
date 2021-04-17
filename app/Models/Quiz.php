8359268e38e58914cafa34c16380d367c4762d9d
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-18 00:23:12 +0300

    Kullanıcı Quiz Listeleme Sayfası Oluşturuldu Quiz Detay Sayfası Oluşturuldu Eloquent Sluggable Eklendi

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -5,20 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 
 class Quiz extends Model
 {
     use HasFactory;
-    protected $fillable=['title','description','finished_at'];
+    use Sluggable;
+
+    protected $fillable=['title','description','finished_at','slug'];
 
     protected $dates=['finished_at'];
 
-    public function getFinishedAtAttribute($date){
+    public function getFinishedAtAttribute($date)
+    {
         return $date ? Carbon::parse($date) : null;
     }
 
     public function questions(){
         return $this->hasMany('App\Models\Question');
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'onUpdate' => true,
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\QuizController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\Admin\QuestionController;
 
 /*
@@ -19,9 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('panel',[MainController::class, 'dashboard'])->name('dashboard');
+    Route::get('quiz/{slug}',[MainController::class, 'quiz_detail'])->name('quiz.detail');
+});
 
 Route::group(['middleware' => ['auth','isAdmin'],'prefix' => 'admin'], function(){
     Route::get('quizzes/{id}',[QuizController::class, 'destroy'])->whereNumber('id')->name('questions.destroy');
