690f738435bec37fff611e68bacd4e5c03c265a7
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-19 02:24:16 +0300

    Answers Tablosu Migration,Seed ve Factory Oluşturuldu

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -4,7 +4,8 @@
     </x-slot>
     <div class="card" >
         <div class="card-body">
-              <form method="POST" action="#">
+              <form method="POST" action="{{route('quiz.result',$quiz->slug)}}">
+              @csrf
               @foreach ($quiz->questions as $question)
 
                   <strong>#{{$loop->iteration}}</strong> {{$question->question}}
