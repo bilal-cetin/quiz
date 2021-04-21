0fe69b72bd93a67f4ebb4027136b5275046b2164
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-22 00:35:40 +0300

    Quiz Analiz Verileri Oluşturuldu

--- resources/views/quiz_detail.blade.php
+++ resources/views/quiz_detail.blade.php
@@ -8,30 +8,49 @@
               <div class="row">
                   <div class="col-md-4">
                     <ul class="list-group">
-                        @if($quiz->finished_at)
+                      @if($quiz->my_result)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                          Puan
+                          <span class="badge badge-primary badge-pill">{{ $quiz->my_result->point }}</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                          Doğru / Yanlış Sayısı
+                          <div class="float-right">
+                            <span class="badge badge-success badge-pill">{{ $quiz->my_result->correct }} Doğru</span>
+                            <span class="badge badge-danger badge-pill">{{ $quiz->my_result->wrong }} Yanlış</span>
+                          </div>
+                        </li>
+                      @endif
+                      @if($quiz->finished_at)
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                             Son Katılım Tarihi
                             <span title="{{$quiz->finished_at}}" class="badge badge-secondary badge-pill">{{$quiz->finished_at->diffForHumans()}}</span>
-                          </li>
-                        @endif
+                        </li>
+                      @endif
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                           Soru Sayısı
                           <span class="badge badge-secondary badge-pill">{{$quiz->questions_count}}</span>
                         </li>
+                      @if($quiz->details)
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                             Katılımcı Sayısı
-                            <span class="badge badge-secondary badge-pill">14</span>
-                          </li>
+                            <span class="badge badge-warning badge-pill">{{ $quiz->details['join_count']}}</span>
+                        </li>
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                           Ortalama Puan
-                          <span class="badge badge-secondary badge-pill">60</span>
+                          <span class="badge badge-light badge-pill">{{$quiz->details['average']}}</span>
                         </li>
-                        
+                      @endif
                       </ul>
                   </div>
                   <div class="col-md-8">
-                    {{$quiz->description}}</p>
+                    {{$quiz->description}}
+          </p>
+                    @if($quiz->my_result)
+                    <a href="{{route('quiz.join',$quiz->slug)}}" class="btn btn-warning btn-block">Quizi Görüntüle</a>
+                    @else
                     <a href="{{route('quiz.join',$quiz->slug)}}" class="btn btn-primary btn-block">Quize Katıl</a>
+                    @endif
                 </div>
               </div>
              
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'auth'], function(){
-    Route::get('panel',[MainController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard',[MainController::class, 'dashboard'])->name('dashboard');
     Route::get('quiz/detay/{slug}',[MainController::class, 'quiz_detail'])->name('quiz.detail');
     Route::get('quiz/{slug}',[MainController::class, 'quiz'])->name('quiz.join');
     Route::post('quiz/{slug}/result',[MainController::class, 'result'])->name('quiz.result');
