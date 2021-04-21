1a87d5c51d2e1b3cb02c9722ec5f8ff2dd6ed222
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-22 02:55:26 +0300

    Kendi Sıralama Bilgileri Eklendi

--- resources/views/quiz_detail.blade.php
+++ resources/views/quiz_detail.blade.php
@@ -8,6 +8,12 @@
               <div class="row">
                   <div class="col-md-4">
                     <ul class="list-group">
+                      @if($quiz->my_rank)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                          Sıralaman
+                          <span class="badge badge-success badge-pill">#{{ $quiz->my_rank }}</span>
+                        </li>
+                      @endif
                       @if($quiz->my_result)
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                           Puan
@@ -50,11 +56,12 @@
                           <ul class="list-group">
                             @foreach ($quiz->topTen as $result)
                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                              <strong class="h3">{{$loop->iteration}}.</strong>
+                              <strong class="h5">{{$loop->iteration}}.</strong>
                               @if($result->user->profile_photo_path)
                               <img class ="w-8 h-8 rounded-full" src="{{$result->user->profile_photo_url}}">
                               @endif
-                              {{$result->user->name}}
+                              
+                              <span @if(auth()->user()->id == $result->user_id) class="text-success" @endif>{{$result->user->name}}</span>
                               <span class="badge badge-success badge-pill">{{$result->point}}</span>
                             </li>
                             @endforeach
