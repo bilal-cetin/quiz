53f882dbfa9d97332a0a331aa64b7c11f5a23e12
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-21 20:11:41 +0300

    Result Tablosu Oluşturuldu Quiz Sonucu Hesaplandı

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Quiz;
+use App\Models\Answer;
+use App\Models\Result;
 
 class MainController extends Controller
 {
@@ -23,6 +25,30 @@ class MainController extends Controller
     }
 
     public function result(Request $request,$slug){
-        return $request;
+        $quiz = Quiz::with('questions')->whereSlug($slug)->first();
+        $correct = 0;
+        
+        foreach($quiz->questions as $question){
+            Answer::create([
+                'user_id'=>auth()->user()->id,
+                'question_id'=>$question->id,
+                'answer'=>$request->post($question->id)
+            ]);
+            
+            if($question->correct_answer===$request->post($question->id)){
+                $correct+=1;
+            }
+        }
+        $point= round((100 / count($quiz->questions)) * $correct);
+        $wrong = count($quiz->questions) - $correct;
+
+        Result::create([
+            'user_id'=>auth()->user()->id,
+            'quiz_id'=>$quiz->id,
+            'point'=>$point,
+            'correct'=>$correct,
+            'wrong'=>$wrong,
+        ]);
+        return redirect()->route('quiz.detail',$quiz->slug)->withSuccess("Başarıyla Quiz'i bitirdin. Puanın : ".$point);
     }
 }
